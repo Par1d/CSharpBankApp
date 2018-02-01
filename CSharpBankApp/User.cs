910589e16e0b8c7f48585b23201fa1ac21427fa7
C Sharp <C Sharp@B056-119>
2018-02-01 11:20:10 -0700

    user

--- CSharpBankApp/User.cs
+++ CSharpBankApp/User.cs
@@ -8,5 +8,85 @@ namespace CSharpBankApp
 {
     class User
     {
+        private string firstName;
+        private string lastName;
+        private string username;
+        private string password;
+
+        public User()
+        {
+        }
+
+        public User(string firstName, string lastName, string username, string password)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Username = username;
+            this.Password = password;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if (value.Length > 50)
+                {
+                    throw new ArgumentException(
+                        "Maximum length of first name is 50 characters.");
+                }
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if (value.Length > 50)
+                {
+                    throw new ArgumentException(
+                        "Maximum length of last name is 50 characters.");
+                }
+                lastName = value;
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                if (value.Length > 50)
+                {
+                    throw new ArgumentException(
+                        "Maximum length of username address is 50 characters.");
+                }
+                username = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return password;
+
+                password = value;
+
+            }
+        }
+        public virtual string GetDisplayText() => firstName + " " + lastName + ", " + username;
     }
 }
+    
+
