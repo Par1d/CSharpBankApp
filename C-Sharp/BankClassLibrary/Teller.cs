2186d4d0e6daa0bae6f3a72b8ca79a88dc9cb5b9
PWolcott96 <35589418+PWolcott96@users.noreply.github.com>
2018-02-16 11:38:06 -0700

    Add files via upload

---
+++ C-Sharp/BankClassLibrary/Teller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankClassLibrary
+{
+    public class Teller
+    {
+        string _tellerID;
+        Vault _cashDrawer;
+    }
+}
