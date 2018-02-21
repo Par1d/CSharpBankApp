ddb6f652c2e6b49062a51706ee4bab83074004c3
Parker Wolcott <parker.wolcott@msubillings.edu>
2018-02-21 15:19:38 -0700

    Added a run code to the program.cs form on CSharpBankApp project so forms would run

--- CSharpBankApp/Program.cs
+++ CSharpBankApp/Program.cs
@@ -16,6 +16,7 @@ namespace CSharpBankApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Dashboard());
         }
     }
 }
