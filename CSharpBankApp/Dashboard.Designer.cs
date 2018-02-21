81c37c97c807d6c392626161cf308dba3376f87f
Parker Wolcott <parker.wolcott@msubillings.edu>
2018-02-21 15:44:59 -0700

    further edit on dashboard

--- CSharpBankApp/Dashboard.Designer.cs
+++ CSharpBankApp/Dashboard.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(360, 284);
+            this.ClientSize = new System.Drawing.Size(395, 338);
             this.Name = "Dashboard";
             this.Text = "Dashboard";
             this.ResumeLayout(false);
