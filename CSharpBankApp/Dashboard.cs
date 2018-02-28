21197f499a0133962ea86b34019a749ccb447dcc
Parker Wolcott <parker.wolcott@msubillings.edu>
2018-02-28 11:44:51 -0700

    changed buttons on dashboard to tab control instead

--- CSharpBankApp/Dashboard.cs
+++ CSharpBankApp/Dashboard.cs
@@ -20,66 +20,9 @@ namespace CSharpBankApp
         private void Dashboard_Load(object sender, EventArgs e)
         {
             new LoginForm().Show();
-            dashboardpnl.Show();
-            Withdrawpnl.Hide();
-            Depositpnl.Hide();
         }
 
-
-        /// Hides the Withdraw Panel and Shows the Deposit Panel
-        private void Depositbtn_Click(object sender, EventArgs e)
-        {
-            Depositpnl.Show();
-            Mainlbl.Show();
-            Withdrawpnl.Hide();
-            dashboardpnl.Hide();
-        }
-
-        /// Hides the Deposit Panel and Shows the Withdraw Panel
-        private void Withdrawbtn_Click(object sender, EventArgs e)
-        {
-            Withdrawpnl.Show();
-            Mainlbl.Show();
-            Depositpnl.Hide();
-            dashboardpnl.Hide();
-        }
-
-        private void Closebtn_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
-
-        private void DepositCancelbtn_Click(object sender, EventArgs e)
-        {
-            Depositpnl.Hide();
-            Mainlbl.Hide();
-            dashboardpnl.Show();
-        }
-
-        private void WithdrawCancelbtn_Click(object sender, EventArgs e)
-        {
-            Withdrawpnl.Hide();
-            Mainlbl.Hide();
-            dashboardpnl.Show();
-        }
-
-        private void DepositClearbtn_Click(object sender, EventArgs e)
-        {
-            DepositAmounttxt.Clear();
-            DepositAmountVerifytxt.Clear();
-            DepositPintxt.Clear();
-            DepositPinVerifytxt.Clear();
-        }
-
-        private void WtihdrawClearbtn_Click(object sender, EventArgs e)
-        {
-            WithdrawAmounttxt.Clear();
-            WithdrawAmountVerifytxt.Clear();
-            WithdrawPintxt.Clear();
-            WithdrawPinVerifytxt.Clear();
-        }
-
-        private void logoutlbl_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             this.Close();
         }
