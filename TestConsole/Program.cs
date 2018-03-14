89266f95790c84ff8dd2ed9ee9df809326da5df0
C Sharp <C Sharp@B056-122>
2018-03-14 11:28:01 -0600

    messing with queue

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -19,7 +19,49 @@ namespace TestConsole
             bob.WithdrawFromAccount(1234, 1234, 100m);
             bob.WithdrawFromAccount(1234, 1234, 200m);
             bob.Accounts[0].DisplayHistory();
+            
+            
+            
+            
+            //messing with queue below
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
 
+
+            Vault v = new Vault();
+            Queue<CirculatingMoney> q = new Queue<CirculatingMoney>();
+
+            Dime n = new Dime(); // n is the variable containing a dime
+            int a = n.GetHashCode(); //dime hashcode is a
+
+            q.Enqueue(new Penny());
+            q.Enqueue(new Penny());
+            q.Enqueue(new Dime());
+            
+            
+
+            Console.WriteLine(q.Count);
+            // Console.WriteLine(q.Peek());
+
+            //check to see if there is a dime in the queue
+             if (q.Contains(1))
+             {
+                 Console.WriteLine("the queue has a dime");
+             }
+             else {
+                 Console.WriteLine("there is no dime");
+
+             }
+            //take off objects in queue
+            int s = q.Count;
+            for (int i = 0; i < s; i++) {
+                Console.WriteLine(q.Dequeue().Value + " this is the " + i +" object");
+                
+
+            }
+            Console.WriteLine(q.Count +" number of objects in queue");
+            v.enterVault(q);
             Console.ReadKey();
         }
     }
